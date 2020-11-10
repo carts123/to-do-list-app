61c3e4e25afa35f6ccfb455026d6eddf699f30d5
carts123 <72449546+carts123@users.noreply.github.com>
2020-11-10 10:46:31 +0000

    Function added on line 1  along with comments for code

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-//1. Adding a new item to the list of items:
+function newitem() {
+
+// Adding a new item to the list of items:
 let li = $('<li></li>');
 let inputValue = $('#input').val();
   li.append(inputValue);
@@ -9,7 +11,7 @@ if (inputValue === '') {
   $('#list').append(li); //didn't fully understand how this section of code works
 }
 
-//2. Crossing out an item from the list of items:
+//Crossing out an item from the list of items:
 function crossOut() {
   li.toggleClass('strike');
 }
@@ -18,7 +20,7 @@ function crossOut() {
     li.toggleClass('strike');
 });
 
-//3(i). Adding the delete button "X":
+//Adding the delete button "X":
 let crossOutButton = $('<crossOutButton></crossOutButton>'); //why is it an open and close tag
   crossOutButton.append(document.createTextNode ('X'));
   li.append(crossOutButton);
@@ -28,3 +30,8 @@ let crossOutButton = $('<crossOutButton></crossOutButton>'); //why is it an open
   function deleteListItem() {
     li.addCLass('delete');
   }
+
+//Items can be resorted
+$('#list').sortable();
+
+}
